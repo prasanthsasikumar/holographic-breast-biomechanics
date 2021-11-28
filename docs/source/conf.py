5b37ccaaa4dbc0eef3de61a114930e0cf0a09482
Prasanth Sasikumar <prasanths99@yahoo.co.in>
2021-11-29 02:14:09 +1300

    remove css

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -54,14 +54,6 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-
-
-html_context = {
-    'css_files': [
-        '_static/theme_overrides.css',  # override wide tables in RTD theme
-        ],
-     }
-
 graphviz_output_format = "svg"
 
 # True to prefix each section label with the name of the document it is in,
